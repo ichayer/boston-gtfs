be8f109af163fd39db9f57988e412ac044ad1699
ichayer <ichayer@itba.edu.ar>
2025-06-18 14:41:40 -0300

    Fix in load_data.sql

--- load_data.sql
+++ load_data.sql
@@ -37,16 +37,16 @@ CREATE TABLE calendar_dates
     FOREIGN KEY (service_id) REFERENCES calendar (service_id)
 );
 
--- https://gtfs.org/documentation/schedule/reference/#feed_infotxt
-DROP TABLE IF EXISTS feed_info CASCADE;
-CREATE TABLE feed_info
+-- https://gtfs.org/documentation/schedule/reference/#shapestxt
+DROP TABLE IF EXISTS shapes CASCADE;
+CREATE TABLE shapes
 (
-    feed_publisher_name TEXT NOT NULL,
-    feed_publisher_url  TEXT NOT NULL,
-    feed_lang           TEXT NOT NULL,
-    feed_start_date     DATE,
-    feed_end_date       DATE,
-    feed_version        TEXT
+    shape_id            TEXT             NOT NULL,
+    shape_pt_lat        DOUBLE PRECISION NOT NULL,
+    shape_pt_lon        DOUBLE PRECISION NOT NULL,
+    shape_pt_sequence   INTEGER          NOT NULL,
+    shape_dist_traveled DOUBLE PRECISION,
+    PRIMARY KEY (shape_id, shape_pt_sequence)
 );
 
 -- https://gtfs.org/documentation/schedule/reference/#routestxt
@@ -54,12 +54,12 @@ DROP TABLE IF EXISTS routes CASCADE;
 CREATE TABLE routes
 (
     route_id         TEXT PRIMARY KEY,
-    agency_id        TEXT    NOT NULL,
+    agency_id        TEXT, -- No reference because its only one agency
     route_short_name TEXT    NOT NULL,
     route_long_name  TEXT    NOT NULL,
-    route_desc       TEXT,
     route_type       INTEGER NOT NULL,
-    FOREIGN KEY (agency_id) REFERENCES agency (agency_id)
+    route_color      TEXT,
+    route_text       TEXT
 );
 
 -- https://gtfs.org/documentation/schedule/reference/#stopstxt
@@ -70,6 +70,7 @@ CREATE TABLE stops
     stop_name      TEXT NOT NULL,
     stop_lat       DOUBLE PRECISION,
     stop_lon       DOUBLE PRECISION,
+    stop_url       TEXT,
     location_type  TEXT,
     parent_station TEXT
 );
@@ -78,15 +79,13 @@ CREATE TABLE stops
 DROP TABLE IF EXISTS trips CASCADE;
 CREATE TABLE trips
 (
-    route_id         TEXT NOT NULL,
-    service_id       TEXT NOT NULL,
-    trip_id          TEXT PRIMARY KEY,
-    trip_headsign    TEXT,
-    trip_short_name  TEXT,
-    direction_id     INTEGER,
-    block_id         TEXT,
-    original_trip_id TEXT, -- Custom field not included in the standard
-    hints            TEXT, -- Custom field not included in the standard
+    route_id      TEXT NOT NULL,
+    service_id    TEXT NOT NULL,
+    trip_id       TEXT PRIMARY KEY,
+    shape_id      TEXT NOT NULL,
+    trip_headsign TEXT,
+    direction_id  INTEGER,
+    block_id      TEXT,
     FOREIGN KEY (route_id) REFERENCES routes (route_id),
     FOREIGN KEY (service_id) REFERENCES calendar (service_id)
 );
@@ -95,13 +94,15 @@ CREATE TABLE trips
 DROP TABLE IF EXISTS stop_times CASCADE;
 CREATE TABLE stop_times
 (
-    trip_id        TEXT    NOT NULL,
-    arrival_time   TEXT    NOT NULL, -- TODO: Check how to parse cases where time >24
-    departure_time TEXT    NOT NULL, -- TODO: Check how to parse cases where time >24
-    stop_id        TEXT    NOT NULL,
-    stop_sequence  INTEGER NOT NULL,
-    pickup_type    INTEGER,
-    drop_off_type  INTEGER,
+    trip_id             TEXT    NOT NULL,
+    arrival_time        TEXT    NOT NULL,
+    departure_time      TEXT    NOT NULL,
+    stop_id             TEXT    NOT NULL,
+    stop_sequence       INTEGER NOT NULL,
+    stop_headsign       TEXT,
+    pickup_type         INTEGER,
+    drop_off_type       INTEGER,
+    shape_dist_traveled DOUBLE PRECISION,
     PRIMARY KEY (trip_id, stop_sequence),
     FOREIGN KEY (trip_id) REFERENCES trips (trip_id),
     FOREIGN KEY (stop_id) REFERENCES stops (stop_id)
@@ -136,13 +137,13 @@ $$
                 );
 
         EXECUTE format(
-                'COPY calendar_dates FROM %L WITH (FORMAT csv, HEADER, DELIMITER '','', QUOTE ''"'')',
-                base_path || 'calendar_dates.txt'
+                'COPY shapes FROM %L WITH (FORMAT csv, HEADER, DELIMITER '','', QUOTE ''"'')',
+                base_path || 'shapes.txt'
                 );
 
         EXECUTE format(
-                'COPY feed_info FROM %L WITH (FORMAT csv, HEADER, DELIMITER '','', QUOTE ''"'')',
-                base_path || 'feed_info.txt'
+                'COPY calendar_dates FROM %L WITH (FORMAT csv, HEADER, DELIMITER '','', QUOTE ''"'')',
+                base_path || 'calendar_dates.txt'
                 );
 
         EXECUTE format(
@@ -168,7 +169,17 @@ $$
                 base_path || 'transfers.txt'
                 );
 
+        -- Stops update
         ALTER TABLE stops
-        ALTER COLUMN location_type TYPE INTEGER USING COALESCE(NULLIF(location_type, ''), '0')::INTEGER;
+            ALTER COLUMN location_type TYPE INTEGER USING COALESCE(NULLIF(location_type, ''), '0')::INTEGER;
+
+        -- Routes update
+        UPDATE routes
+        SET agency_id = (SELECT agency_id FROM agency LIMIT 1);
+
+        ALTER TABLE routes
+            ADD CONSTRAINT fk_routes_agency
+                FOREIGN KEY (agency_id)
+                    REFERENCES agency (agency_id);
     END
 $$;
